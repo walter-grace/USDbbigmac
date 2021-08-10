54798aae40520ae09c281f1370b065ee8ae49898
walter-grace <nico.zahniser@gmail.com>
2021-08-09 21:52:17 -0700

    update

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Hello this is the Big Mac Index</h1>
+      <h3>USD Big Mac</h3>
+      <h2>Start moving around the object art</h2>
+    </div>
+  );
+}
