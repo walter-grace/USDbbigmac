efc1a7aa2448c25924459082c1576b126272b5ee
walter-grace <nico.zahniser@gmail.com>
2021-08-12 08:30:02 -0700

    updated links

--- src/App.js
+++ src/App.js
@@ -3,9 +3,12 @@ import "./styles.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello this is the Big Mac Index</h1>
-      <h3>USD Big Mac</h3>
-      <h2>Start moving around the object art</h2>
+      <h1>Burgernomics</h1>
+      <h3>
+      The big mac index was invented by The Economist in 1986 as a lighthearted guide to whether currencies are at their “correct” level. 
+      It is based on the theory of purchasing-power parity (PPP).
+      </h3>
+      <h2>Click the AR logo and bring the burgers to life!</h2>
     </div>
   );
 }
